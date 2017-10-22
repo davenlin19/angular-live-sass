c104031472eb15190066a9ff74ddbec0e54bdb9c
Trong Lam Phan <lampt2509@gmail.com>
2017-10-22 20:18:25 +0200

    Get selector from command line

--- live-sass/gulpfile.js
+++ live-sass/gulpfile.js
@@ -3,19 +3,42 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
+var injectString = require('gulp-inject-string');
+var argv = require('yargs').argv;
 
-gulp.task('livesass', ['inject'], function() {
-	return gulp.src('../live-sass/removestyle.js')
+console.log(argv.selector);
+
+/*
+	Copy livesass.js to dist/live-sass
+	Dependence: inject
+*/
+gulp.task('copy', ['inject'], function() {
+	return gulp.src('./livesass.js')
 			.pipe(gulp.dest('../dist/live-sass/'));
 });
 
+/*
+	Inject livesass.js into index.html
+*/
 gulp.task('inject', function() {
 	return gulp.src('../src/index.html')
 			.pipe(
 				inject(
-					gulp.src('./removestyle.js', {read: false})
+					gulp.src('./livesass.js', {read: false})
 						.pipe(gulp.dest('live-sass'))
 				)
 			)
 			.pipe(gulp.dest('../src'));
+});
+
+/*
+	Define component's selector
+	Dependece: copy
+*/
+gulp.task('livesass', ['copy'], function() {
+	if (argv.selector !== undefined) {
+		gulp.src('../dist/live-sass/livesass.js')
+			.pipe(injectString.prepend('var selector = \'' + argv.selector + '\';\n\n'))
+			.pipe(gulp.dest('../dist/live-sass/'));
+	}
 });
